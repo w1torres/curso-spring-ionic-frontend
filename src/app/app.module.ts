b85df463559ea2a0e10fd8f8e379af45c074e739
Wildes Torres da Silva <wildes.silva@serpro.gov.br>
2023-05-04 14:32:18 -0300

    Salvando dados do usuário logado no localStorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { CategoriaService } from '../services/domain/categoria.service';
 import { ErrorInterceptorProvider } from '../intercepetors/error-interceptor';
 import { AuthService } from '../services/domain/auth.services';
+import { StorageService } from '../services/storage.service';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { AuthService } from '../services/domain/auth.services';
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoriaService,
     ErrorInterceptorProvider,
-    AuthService
+    AuthService,
+    StorageService
   ]
 })
 export class AppModule {}
