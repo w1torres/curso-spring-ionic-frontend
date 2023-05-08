1c58dcd40835360ce4488084a56ff6a8474d2df8
Wildes Torres da Silva <wildes.silva@serpro.gov.br>
2023-05-08 14:17:27 -0300

    Carregando produtos de uma dada categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AuthService } from '../services/auth.services';
 import { StorageService } from '../services/storage.service';
 import { ClienteService } from '../services/domain/cliente.service';
 import { AuthInterceptorProvider } from '../intercepetors/auth-interceptor';
+import { ProdutoService } from '../services/domain/produto.service';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { AuthInterceptorProvider } from '../intercepetors/auth-interceptor';
     ErrorInterceptorProvider,
     AuthService,
     StorageService,
-    ClienteService
+    ClienteService,
+    ProdutoService
   ]
 })
 export class AppModule {}
--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
+import { ProdutoService } from '../../services/domain/produto.service';
 
 @IonicPage()
 @Component({
@@ -11,10 +12,19 @@ export class ProdutosPage {
 
   items: ProdutoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public produtoSevice: ProdutoService) {
   }
 
   ionViewDidLoad() {
+    let categoria_id = this.navParams.get('categoria_id');
+    this.produtoSevice.findByCategoria(categoria_id)
+      .subscribe(response => {
+        this.items = response['content'];
+      })
+
     this.items = [
       {
           id: "1",
