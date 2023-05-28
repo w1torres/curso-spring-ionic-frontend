fd0522b860ab4cff8e72c1d72daefc6357791216
Wildes Torres da Silva <wildes.silva@serpro.gov.br>
2023-05-28 12:28:07 -0300

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -34,6 +34,10 @@ export class ProfilePage {
 
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData(){
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email)
@@ -77,5 +81,18 @@ export class ProfilePage {
       this.cameraOn = false;
     }, (err) => {
     });
+  }
+
+  sendPicture(){
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response =>{
+        this.picture = null;
+        this.loadData();
+      },
+      error => {})
+  }
+
+  cancel(){
+    this.picture = null;
   }
 }
